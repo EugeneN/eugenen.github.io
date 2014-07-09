f482e52914069b987ae3b0c5419ac2a4215b37cd
Eugene <eugene.naumenko@gmail.com>
2014-07-10 01:33:40 +0300

    css & html work

--- resources/public/js/cmd/cmd/ui.cljs
+++ resources/public/js/cmd/cmd/ui.cljs
@@ -3,6 +3,8 @@
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [chan close! >! <!]]
 
+            [goog.style :as gstyle]
+
             [cmd.utils :refer [say html! join-gist-names setcookie getcookie]]
             [cmd.core :refer [set-state reset-state get-state process load-gists load-gist
              save-gist authenticate authenticated-om? error-set? state AppBus]])
@@ -48,10 +50,19 @@ To begin:
 
 (defn reset-input [] (ace-set-value motd))
 
+(defn process-cb
+  [value]
+  (do
+    (html! preview value)
+    ;(js/setTimeout
+    ;  #(.Queue js/MathJax.Hub ["Typeset" (.-Hub js/MathJax) "preview"])
+    ;  300)
+    ))
+
 (defn set-preview []
   (let [ace (get-state state :ace)
         ace-value (.. ace (getSession) (getValue))]
-    (process ace-value #(html! preview %))))
+    (process ace-value process-cb)))
 
 (defn handle-pull
   [_]
@@ -70,7 +81,7 @@ To begin:
 (defn handle-logout
   [_]
   (do
-    (say "Logout")
+    (say "Bye bye")
     (reset-state state)
     (setcookie "username" "")
     (setcookie "auth-token" "")
@@ -105,8 +116,6 @@ To begin:
 
             (let [current-gist (state :current-gist)
                   href (if (= current-gist nil) nil (current-gist "html_url"))]
-              (say current-gist)
-              (say href)
               (if (not (= href nil))
                 (dom/a #js {:id "view-orig"
                             :target "_blank"
@@ -146,24 +155,74 @@ To begin:
 
 ; main section -----------------------------------------------------------------
 
-(defn setup-editor-listeners
+(defn $ [id-str] (.getElementById js/document id-str))
+(defn visible? [el] (.isElementShown goog.style el))
+(defn set-width [el width] (set! (.. el -style -width) width))
+(defn toggle [el] (.showElement goog.style el (not (visible? el))))
+
+(defn setup-toolbar-listeners
   []
-  (let [session (.. (get-state state :ace) (getSession))]
+  (let [toolbar-toggler   ($ "toolbar-toggler")
+        editor-toggler    ($ "editor-toggler")
+        toolbar           ($ "toolbar")
+        preview           ($ "preview-container")
+        preview-container ($ "outer-preview-container")
+        editor            ($ "input")
+        preview-toggler   ($ "preview-toggler")]
 
     (.. js/Rx -Observable
-      (create (fn [observer] (.. session (on "changeScrollTop" #(.. observer (onNext %))))))
-      (throttle 15)
-      (subscribe #(set! (.-scrollTop preview-container) %)))
+      (fromEvent toolbar-toggler "click")
+      (subscribe #(toggle toolbar)))
+
+    (.. js/Rx -Observable
+      (fromEvent preview-toggler "click")
+      (subscribe #(do
+                   (toggle preview)
+                   (if (visible? preview)
+                     (set-width preview-container "49%")
+                     (set-width preview-container "20px")))))
+
+    (.. js/Rx -Observable
+      (fromEvent editor-toggler "click")
+      (subscribe #(toggle editor)))
+
+    ))
+
+(defn setup-editor-listeners
+  []
+  (let [editor (get-state state :ace)
+        session (.. editor (getSession))]
 
     (.. js/Rx -Observable
       (create (fn [observer] (.. session (on "change" #(.. observer (onNext))))))
       (throttle 300)
       (subscribe #(set-preview)))
 
+    ;editor.getSession().getScreenLength()
+                  * editor.renderer.lineHeight
+
+    (defn calc-offset-top-preview
+      [ot1]
+      (let [ch1 (* (.. session (getScreenLength)) (.. editor -renderer -lineHeight))
+            ch2 (.-clientHeight preview)]
+        (/ (* ot1 ch2) ch1)))
+
+    (defn calc-offset-top-input
+      [ot2]
+      (let [ch1 (* (.. session (getScreenLength)) (.. editor -renderer -lineHeight))
+            ch2 (.-clientHeight preview)]
+        (/ (* ot2 ch1) ch2)))
+
+    (.. js/Rx -Observable
+      (create (fn [observer] (.. session (on "changeScrollTop" #(.. observer (onNext %))))))
+      (throttle 15)
+      (subscribe #(set! (.-scrollTop preview-container) (calc-offset-top-preview %))))
+
+
     (.. js/Rx -Observable
       (fromEvent preview-container "scroll")
       (throttle 15)
-      (subscribe #(.. session (setScrollTop (.-scrollTop preview-container)))))
+      (subscribe #(.. session (setScrollTop (calc-offset-top-input (.-scrollTop preview-container))))))
     ))
 
 (defn setup-ace
@@ -193,6 +252,7 @@ To begin:
     (subscribe-appbus app-bus)
     (setup-ace)
     (setup-editor-listeners)
+    (setup-toolbar-listeners)
 
     (authenticate username auth-token)
 
@@ -207,5 +267,3 @@ To begin:
 
 ; Entry point ------------------------------------------------------------------
 (main state AppBus)
-
-
