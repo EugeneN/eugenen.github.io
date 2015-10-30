e8390a8ad798867d5b8a8bc3d8245e204bf4b57d
Eugene <eugene.naumenko@gmail.com>
2015-10-30 21:11:36 +0200

    Cleanup

--- resources/public/js/cmd/cmd/defs.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 0.0-2202
-goog.provide('cmd.defs');
-goog.require('cljs.core');
-cmd.defs.local_motd = document.getElementById("motd").text;
-cmd.defs.default_title = "C.MD";
-cmd.defs.default_motd_id = "58a15db96ca12b952f8e";
-cmd.defs.all_panels = new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 4, ["e",null,"p",null,"t",null,"c",null], null), null);
-
-//# sourceMappingURL=defs.js.map
