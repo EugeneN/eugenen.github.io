38fd96cb1ea3fe3093e8b4f9222cc3b67d047dd8
Eugene Naumenko <eugene.naumenko@gmail.com>
2019-02-01 13:26:35 +0200

    spin the globe

---
+++ spin-the-globe/geoHelpers.js
@@ -0,0 +1,147 @@
+
+ function getPoint(event) {
+
+  // Get the vertices
+  let a = this.geometry.vertices[event.face.a];
+  let b = this.geometry.vertices[event.face.b];
+  let c = this.geometry.vertices[event.face.c];
+
+  // Averge them together
+  let point = {
+    x: (a.x + b.x + c.x) / 3,
+    y: (a.y + b.y + c.y) / 3,
+    z: (a.z + b.z + c.z) / 3
+  };
+
+  return point;
+}
+
+ function getEventCenter(event, radius) {
+  radius = radius || 200;
+
+  var point = getPoint.call(this, event);
+
+  var latRads = Math.acos(point.y / radius);
+  var lngRads = Math.atan2(point.z, point.x);
+  var lat = (Math.PI / 2 - latRads) * (180 / Math.PI);
+  var lng = (Math.PI - lngRads) * (180 / Math.PI);
+
+  return [lat, lng - 180];
+}
+
+function convertToXYZ(point, radius) {
+  var lambda = point[0] * Math.PI / 180,
+      phi = point[1] * Math.PI / 180,
+      cosPhi = Math.cos(phi);
+  return new THREE.Vector3(
+    radius * cosPhi * Math.cos(lambda),
+    radius * Math.sin(phi),
+  - radius * cosPhi * Math.sin(lambda)
+  );
+}
+function rad2deg(point) {
+   var latD = point[0] * 180 / Math.PI;
+   var lonD = point[1] * 180 / Math.PI;
+
+   return {x: latD, y: lonD, z: 0};
+}
+
+function deg2rad(point) {
+  var latR = point[0] * Math.PI / 180,
+      lonR = point[1] * Math.PI / 180;
+
+  return {x: latR, y: -Math.PI/2 - lonR, z: 0};
+}
+
+function convertToXYZ_(point, radius) {
+
+  var latRads = ( 90 - point[0]) * Math.PI / 180;
+  var lngRads = (180 - point[1]) * Math.PI / 180;
+
+  var x = radius * Math.sin(latRads) * Math.cos(lngRads);
+  var y = radius * Math.cos(latRads);
+  var z = radius * Math.sin(latRads) * Math.sin(lngRads);
+
+  return {x: x, y: y, z: z};
+}
+
+ function geodecoder (features) {
+
+  let store = {};
+
+  for (let i = 0; i < features.length; i++) {
+    store[features[i].id] = features[i];
+  }
+
+  return {
+    show: function() {
+      return store;
+    },
+    getRandomCountry: function() {
+      var xs = Object.values(store);
+      var min = 0, max = xs.length;
+      var i = Math.round(Math.random() * (max - min) + min);
+      return xs[i];
+    },
+    find: function (id) {
+      return store[id];
+    },
+    search: function (lat, lng) {
+
+      let match = false;
+
+      let country, coords;
+
+      for (let i = 0; i < features.length; i++) {
+        country = features[i];
+        if(country.geometry.type === 'Polygon') {
+          match = pointInPolygon(country.geometry.coordinates[0], [lng, lat]);
+          if (match) {
+            return {
+              code: features[i].id,
+              name: features[i].properties.name
+            };
+          }
+        } else if (country.geometry.type === 'MultiPolygon') {
+          coords = country.geometry.coordinates;
+          for (let j = 0; j < coords.length; j++) {
+            match = pointInPolygon(coords[j][0], [lng, lat]);
+            if (match) {
+              return {
+                code: features[i].id,
+                name: features[i].properties.name
+              };
+            }
+          }
+        }
+      }
+
+      return null;
+    }
+  };
+};
+
+// http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+function pointInPolygon(poly, point) {
+
+  let x = point[0];
+  let y = point[1];
+
+  let inside = false, xi, xj, yi, yj, xk;
+
+  for (let i = 0, j = poly.length - 1; i < poly.length; j = i++) {
+    xi = poly[i][0];
+    yi = poly[i][1];
+    xj = poly[j][0];
+    yj = poly[j][1];
+
+    xk = ((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    if (xk) {
+       inside = !inside;
+    }
+  }
+
+  return inside;
+};
+
+
---
+++ spin-the-globe/mapTexture.js
@@ -0,0 +1,43 @@
+
+var projection = d3.geoEquirectangular()
+  .translate([2048, 1024])
+  .scale(650);
+
+ function mapTexture(geojson, fgColor, bgColor) {
+  var texture, context, canvas;
+
+  canvas = d3.select("body").append("canvas")
+    .style("display", "none")
+    .attr("width", "4096px")
+    .attr("height", "2048px");
+
+  context = canvas.node().getContext("2d");
+
+  var path = d3.geoPath()
+    .projection(projection)
+    .context(context);
+
+  context.strokeStyle = fgColor; 
+  context.lineWidth = 1;
+  context.fillStyle = bgColor; 
+
+  context.beginPath();
+
+  path(geojson);
+
+  if (bgColor) {
+    context.fill();
+  }
+
+  context.stroke();
+
+  // DEBUGGING - Really expensive, disable when done.
+  // console.log(canvas.node().toDataURL());
+
+  texture = new THREE.Texture(canvas.node());
+  texture.needsUpdate = true;
+
+  canvas.remove();
+
+  return texture;
+}
